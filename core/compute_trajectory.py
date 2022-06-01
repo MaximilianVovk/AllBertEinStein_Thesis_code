895453ae0a55a1c79f2e87a19c068a990617cc3f
Maximilian Vovk <maximilian.vovk@tum.de>
2022-06-01 12:41:08 +0200

    Replace compute_trajectory.py with Max code

--- core/compute_trajectory.py
+++ core/compute_trajectory.py
@@ -1,4 +1,4 @@
-#==================================================================================================
+# ==================================================================================================
 # file compute_trajectory.py
 #
 # Computing the behavior of the reentry of a compact object for the AllBert EinStein mission
@@ -9,13 +9,15 @@
 # V1.1, 2017-10-29, dvk: update to run 3-D parameter space
 # V1.2, 2017-10-31, dvk: updated to plot as function of final radius
 # V2.0, 2021-11-27, dvk: apply to AllBert EinStein - added doc strings
-#==================================================================================================
+# V2.1, 2022-05-21, MAX: added variable/out of stationary 3D observer position
+# ==================================================================================================
 
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 # import libraries
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
+#from pyatmos import coesa76
 
 
 def compute_trajectory(atmo_density, r_planet, g0_planet, a, drag_coeff, heat_transfer,
@@ -78,10 +80,13 @@ def compute_trajectory(atmo_density, r_planet, g0_planet, a, drag_coeff, heat_tr
         An array with all time stamps of the computed points, in s.
 
     sa: nparray of float
+        An array with all the horizontal movement simulating the ground track
 
     ha: nparray of float
+        An array with all the altitude of the object
 
     ra: nparray of float
+        An array with all the altitude of the object plus the earth radius
 
     va: nparray of float
         An array with the veloicty of the object as a function of time
@@ -89,48 +94,54 @@ def compute_trajectory(atmo_density, r_planet, g0_planet, a, drag_coeff, heat_tr
     thetaa: nparray of float
         An array with the flight path angle (w.r.t. horizontal) as a function of time
 
-    a2a:
-        tbd
+    a2a: nparray of float
+        Drag deceleration of the object
         
-    ma:
-        tbd
+    ma: nparray of float
+        Current mass of the object
         
-    mva:
-        tbd
+    mva: nparray of float
+        visual magnitude of the object
         
-    zda:
-        tbd
+    zda: nparray of float
+        Zenith angle of the velocity direction
         
 
     To do:
     ------
-    - Make the lum_eff a function of e.g. velocity.
-    - No fragmentation included.
+    #todo - variation of observer position.
+    #todo - Make the lum_eff a function of e.g. velocity.
+    #todo - No fragmentation included.
         
     """
+
     # Helper function
     def init_array():
         x = np.empty(array_length)
         x[:] = np.nan
         return x
 
+    sg = heat_transfer / (2 * drag_coeff * heat_of_ablation)  # ????????????????????????????????????
 
-    sg = heat_transfer / (2 * drag_coeff * heat_of_ablation) #????
+    h = 120000.0  # initial altitude in m
+    s_init = 0.0  # initial position of the observer
+    s = 0.0  # starting position of the asteroid ground track
+    l = 0.0  # distance offtrack position of the observer
+    t = 0.0  # initial time
+    fm = 1  # output will be printed every fm time steps
+    mvmax = 9.0  # faintest magnitude to be printed
 
-    h = 120000.0                         # initial altitude in m
-    s = 0.0                              # initial ground track
-    t = 0.0                              # initial time
-    fm = 1                               # output will be printed every fm time steps
-    mvmax = 9.0                          # faintest magnitude to be printed
+
+    h_dist = np.sqrt((s-s_init) ** 2 + (h-h_obs) ** 2 + l ** 2)  # initial observer distance
 
     f1 = fm
-    m = mass_init                        # intial mass is set
-    v = v_init * 1000.                   # convert velocity to m/s
-    zr = z_init * np.pi / 180.           # convert zenith angle to radians
-    v_vertical = -v * np.cos(zr)         # initial velocity in vertical direction
-    v_horizontal = v * np.sin(zr)        # initial velocity in horizontal direction
+    m = mass_init  # intial mass is set
+    v = v_init * 1000.  # convert velocity to m/s
+    zr = z_init * np.pi / 180.  # convert zenith angle to radians
+    v_vertical = -v * np.cos(zr)  # initial velocity in vertical direction
+    v_horizontal = v * np.sin(zr)  # initial velocity in horizontal direction
 
-    first_time = 1              # flag to indicate that we run the first time
+    first_time = 1  # flag to indicate that we run the first time
 
     # Initalize arrays for output
     ta = init_array()
@@ -138,49 +149,53 @@ def compute_trajectory(atmo_density, r_planet, g0_planet, a, drag_coeff, heat_tr
     ha = init_array()
     ra = init_array()
     va = init_array()
-    thetaa = init_array()
+    #thetaa = init_array()
     a2a = init_array()
     ma = init_array()
     mva = init_array()
     zda = init_array()
+    h_dista = init_array()
 
-    #----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
     # do the computation
-    #----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
     for step in np.arange(0, array_length):
         # find density of atmosphere from lookup table. The table is in steps of 10 km.
-        i = int(np.floor(h/10000.))
-        if i > 15:                                            # too high for noticeable atmosphere
+        i = int(np.floor(h / 10000.))
+        if i > 15:  # too high for noticeable atmosphere
             i = 15
         fr = (h / 10000.) - i
 
         # interpolate current density, 1 is the number one (not al 'el')
-        rho_air =  10**(atmo_density[i] * (1 - fr) + fr * atmo_density[i + 1])
+        # coesa76_geom = coesa76(h/1000) # a bit longer
+        # rho_air = coesa76_geom.rho
+        rho_air = 10 ** (atmo_density[i] * (1 - fr) + fr * atmo_density[i + 1])
 
         # calculate rates of change
-        a2 = drag_coeff * a * rho_air * v * v / (m * rho_object * rho_object)**0.33333
-        gv = g0_planet / (1 + h / r_planet)**2
-        av = -gv - a2 * v_vertical / v + v_horizontal * v_horizontal / (r_planet + h)
-        ah = -a2 * v_horizontal / v - v_vertical * v_horizontal / (r_planet + h)
-        m0 = sg * m * v * a2                                            # mass loss
-        zd = np.arctan2(v_vertical, v_horizontal) * 180. / np.pi        # compute new zenith angle
+        a2 = drag_coeff * a * rho_air * v * v / (m * rho_object * rho_object) ** 0.33333  # deceleration same formula
+        gv = g0_planet / (1 + h / r_planet) ** 2  # gravity at that height
+        av = -gv - a2 * v_vertical / v + v_horizontal * v_horizontal / (r_planet + h)  # gravity + acceleration vertical + centrifugal
+        ah = -a2 * v_horizontal / v - v_vertical * v_horizontal / (r_planet + h)  # acceleration horizontal + centrifugal
+        # m0 = sg * m * v * a2  # mass loss in time
+        m0 = heat_transfer * a * rho_air * v * v * v * ((m / rho_object)**0.66667) / (2 * heat_of_ablation)
+        zd = np.arctan2(v_vertical, v_horizontal) * 180. / np.pi  # compute new zenith angle
         if (f1 == fm):
             # calculate visual magnitude via empirical formula
-            luminous_energy = 0.5 * v * v * m0 * lum_eff * 1e10 / (h_obs * h_obs)
+            luminous_energy = 0.5 * v * v * m0 * lum_eff * 1e10 / (h_dist * h_dist)
             if luminous_energy < 0.1:
                 mv = np.nan
             else:
-                mv = 6.8 - 2.086 * np.log10(luminous_energy)
+                mv = 6.8 - 1.086 * np.log10(luminous_energy)  # magnitude before 2.086 but in -1.086 kampbel-brown & koshny
 
             if mv < mvmax:
                 if first_time == 1:
                     first_time = 0
                     t = 0.
-                
+
                 # print output
                 # print('%7.1f  %8.1f   %6.1f  %6.1f  %7.1f   %6.1f %5.1f  %6.1f' %
                 # (t, s/1000., h/1000., v/1000., a2, 100. * m / mass_init, mv, zd))
-                
+
             # assign to output arrays
             ta[step] = t
             sa[step] = s
@@ -191,31 +206,33 @@ def compute_trajectory(atmo_density, r_planet, g0_planet, a, drag_coeff, heat_tr
             ma[step] = m
             mva[step] = mv
             zda[step] = zd
+            h_dista[step] = h_dist
             # end if mv < mvmax
             f1 = 0
         # endif f1 == fm
-    
+
         # increment values
         t = t + dt
         s = s + v_horizontal * dt * r_planet / (r_planet + h)  # ground track
-        h = h + v_vertical * dt                                # height
-        m = m - m0 * dt                                        # new mass
+        h = h + v_vertical * dt  # height
+        h_dist = np.sqrt((s-s_init) ** 2 + (h-h_obs) ** 2 + l ** 2)  # distance respec to the observer
+        m = m - m0 * dt  # new mass
         v_vertical = v_vertical + av * dt
         v_horizontal = v_horizontal + ah * dt
         v = np.sqrt(v_horizontal * v_horizontal + v_vertical * v_vertical)
         f1 = f1 + 1
-        
+
         # exit criteria
         if h < 0:
-        	# height in m
-        	print(f"Initial mass: {mass_init} kg - height below 0. Final mass: {m} kg")
-        	break
-        
-        if np.sqrt(v_vertical**2 + v_horizontal**2) < 55:
-        	# velocity in m/s
-        	print("velocity below 200 km/h")
-        	break
-        	
+            # height in m
+            print(f"Initial mass: {mass_init} kg - height below 0. Final mass: {m} kg")
+            break
+
+        if np.sqrt(v_vertical ** 2 + v_horizontal ** 2) < 55:
+            # velocity in m/s
+            print("velocity below 200 km/h")
+            break
+
         if mv > mvmax:
             # magnitude below limit
             print("magnitude below limit.")
@@ -225,4 +242,4 @@ def compute_trajectory(atmo_density, r_planet, g0_planet, a, drag_coeff, heat_tr
     # 360 deg it starts again at 0 deg.
     thetaa = [(s / r_planet) % (2 * np.pi) for s in sa]
 
-    return ta, sa, ha, ra, va, thetaa, a2a, ma, mva, zda
+    return ta, sa, ha, ra, va, thetaa, a2a, ma, mva, zda, h_dista
